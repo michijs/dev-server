21ee064390d8aa993ad52f3f48c65532f1dbb0cd
Lucas Segurado <lsegurado1996@gmail.com>
2023-04-13 11:32:39 +0200

    Updated some dependencies

--- src/actions/generateIcons.ts
+++ src/actions/generateIcons.ts
@@ -1,14 +1,15 @@
-import sharp from 'sharp';
 import { getPath } from '../utils/getPath.js';
 import { config } from '../config/config.js';
 import { mkdirSync, existsSync } from 'fs';
-import ico from 'sharp-ico';
 
 export async function generateIcons(callback: () => void, src: string) {
-  const sizes = [24, 48, 72, 96, 128, 256, 512];
-  const image = sharp(src);
   const destPath = getPath(`${config.public.path}/assets/generated`);
   if (!existsSync(destPath)) mkdirSync(destPath, { recursive: true });
+
+  const { default: sharp } = await import('sharp');
+  const { sharpsToIco } = await import('sharp-ico');
+  const sizes = [24, 48, 72, 96, 128, 256, 512];
+  const image = sharp(src);
 
   await Promise.all([
     ...sizes.map((x) => {
@@ -21,7 +22,7 @@ export async function generateIcons(callback: () => void, src: string) {
           ),
         );
     }),
-    ico.sharpsToIco([image], getPath(`${config.public.path}/favicon.ico`), {
+    sharpsToIco([image], getPath(`${config.public.path}/favicon.ico`), {
       sizes: [16],
       resizeOptions: { width: 16, height: 16 },
     }),
