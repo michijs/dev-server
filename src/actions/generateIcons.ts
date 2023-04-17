f81042c3b7396a7e9147d5ae5e209cb72a0cb628
Lucas Segurado <lsegurado1996@gmail.com>
2023-04-17 12:20:28 +0200

    Removed sharp-ico dependency

--- src/actions/generateIcons.ts
+++ src/actions/generateIcons.ts
@@ -1,13 +1,27 @@
 import { getPath } from '../utils/getPath.js';
 import { config } from '../config/config.js';
-import { mkdirSync, existsSync } from 'fs';
+import { mkdirSync, existsSync, writeFileSync } from 'fs';
+import { pngToIco } from '../utils/pngToIco.js';
+
+const generateFavicon = async (src: string, dest: string) => {
+  const { default: sharp } = await import('sharp');
+  const image = await sharp(src);
+
+  const iconSizes = [16, 24, 32, 48, 64, 128, 256];
+  const resizedBuffers = await Promise.all(
+    iconSizes.map((size) => {
+      return image.resize(size, size).png().toBuffer();
+    }),
+  );
+
+  writeFileSync(dest, await pngToIco(resizedBuffers, iconSizes));
+};
 
 export async function generateIcons(callback: () => void, src: string) {
   const destPath = getPath(`${config.public.path}/assets/generated`);
   if (!existsSync(destPath)) mkdirSync(destPath, { recursive: true });
 
   const { default: sharp } = await import('sharp');
-  const { sharpsToIco } = await import('sharp-ico');
   const sizes = [24, 48, 72, 96, 128, 256, 512];
   const image = sharp(src);
 
@@ -22,10 +36,7 @@ export async function generateIcons(callback: () => void, src: string) {
           ),
         );
     }),
-    sharpsToIco([image], getPath(`${config.public.path}/favicon.ico`), {
-      sizes: [16],
-      resizeOptions: { width: 16, height: 16 },
-    }),
+    generateFavicon(src, getPath(`${config.public.path}/favicon.ico`)),
   ]);
 
   callback();
---
+++ src/utils/pngToIco.ts
@@ -0,0 +1,46 @@
+const MaxSize = 256; // 1 << 8
+const MaxFiles = 65536; // 1 << 16
+
+export async function pngToIco(files: Buffer[], iconSizes: number[]) {
+  if (files.length > MaxFiles)
+    throw new Error(`It exceeds the maximum amount of files of ${MaxFiles}`);
+  if (iconSizes.some((x) => x > MaxSize))
+    throw new Error(`It exceeds the maximum icon size of ${MaxSize}`);
+
+  const fileHeader = Buffer.from([0, 0, 1, 0, ...to2Bytes(files.length)]);
+
+  let pngHeaderPos = fileHeader.byteLength + 16 * files.length;
+  const icoBuffer = Buffer.concat([
+    fileHeader,
+    // Every png header
+    ...files.map((x, i) => {
+      const buffer = Buffer.from([
+        iconSizes[i],
+        iconSizes[i],
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        ...to4Bytes(x.byteLength),
+        ...to4Bytes(pngHeaderPos),
+      ]);
+      pngHeaderPos += x.byteLength;
+
+      return buffer;
+    }),
+    // Every png file
+    ...files,
+  ]);
+
+  return icoBuffer;
+}
+
+function to2Bytes(n: number): number[] {
+  return [n & 255, (n >> 8) & 255];
+}
+
+function to4Bytes(n: number): number[] {
+  return [...to2Bytes(n), (n >> 16) & 255, (n >> 24) & 255];
+}
