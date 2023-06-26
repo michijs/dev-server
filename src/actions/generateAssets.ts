e025c4ff3cc4aa22d3c1325f324cd0b5519c0e01
Lucas Segurado <lsegurado1996@gmail.com>
2023-06-26 23:45:32 +0200

    Change requested by netlify

--- src/actions/generateAssets.ts
+++ src/actions/generateAssets.ts
@@ -146,7 +146,7 @@ export async function generateScreenshots() {
                 : "",
             ) {
               const screenshotPath = getPath(
-                `${screenshotsPath}${pageSuffix}${path.replace("?", "_")}`,
+                `${screenshotsPath}${pageSuffix}${path.replaceAll(/[?#]/g, "_")}`,
               );
               if (!existsSync(screenshotPath))
                 mkdirSync(screenshotPath, { recursive: true });
