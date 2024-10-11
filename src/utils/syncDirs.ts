0fd2c56a47ca97e2533652a94563100535a79be4
Lucas Segurado <lsegurado1996@gmail.com>
2024-10-11 20:37:23 +0200

    Bugfix

--- src/utils/syncDirs.ts
+++ src/utils/syncDirs.ts
@@ -24,18 +24,23 @@ export const syncDirs = (
       onStartSync?.();
       const fileSrcDir = path.dirname(fileChangedPath);
       const fileName = path.basename(fileChangedPath);
-      const fileOutDir = fileSrcDir.replace(srcDir, outDir);
-      const pathToRemove = getPath(`${fileOutDir}/${fileName}`);
+      const fileOutDir = fileSrcDir.replace(path.normalize(srcDir), outDir);
       console.log(`  ${coloredString(`File ${fileChangedPath} ${event}.`)}`);
-      if (event === "remove")
-        transformers.forEach((x) => {
-          if (x.fileRegex.test(fileChangedPath)) {
-            const finalPathToRemove =
-              x.pathTransformer?.(pathToRemove) ?? pathToRemove;
-            fs.rmSync(finalPathToRemove, { force: true, recursive: true });
-          }
-        });
-      else {
+      if (event === "remove") {
+        const pathToRemove = getPath(`${fileOutDir}/${fileName}`);
+        const fileTransformer = transformers?.filter((x) => x.fileRegex.test(fileName));
+        if (fileTransformer.length > 0) {
+          fileTransformer.forEach(async (x) => {
+            if (x.fileRegex.test(fileChangedPath)) {
+              const finalPathToRemove =
+                x.pathTransformer?.(pathToRemove) ?? pathToRemove;
+              fs.rmSync(finalPathToRemove, { force: true, recursive: true });
+            }
+          });
+        }
+        else
+          fs.rmSync(pathToRemove, { force: true, recursive: true });
+      } else {
         copyFile(fileSrcDir, fileName, fileOutDir, transformers, omit);
       }
 
