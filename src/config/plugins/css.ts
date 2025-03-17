c443f603407bac2541bd1ee430299a06299a25f3
Lucas Segurado <lsegurado1996@gmail.com>
2025-03-17 20:25:27 +0100

    Small change

--- src/config/plugins/css.ts
+++ src/config/plugins/css.ts
@@ -1,11 +1,21 @@
+import type { Plugin } from "esbuild";
 import { build as esbuild } from "esbuild";
 import { config } from "../config.js";
+import path from "path";
 
-export const cssPlugin = {
+export const cssPlugin: Plugin = {
   name: 'css import assertions',
   setup(build) {
     build.onLoad({ filter: /\.css$/ }, async (args) => {
       try {
+        // Check if CSS is from a library (inside node_modules)
+        let layerName: string | undefined;
+        const parts = args.path.split(path.sep);
+
+        const nodeModulesIndex = parts.lastIndexOf('node_modules');
+        if (nodeModulesIndex !== -1 && parts[nodeModulesIndex + 1]) {
+          layerName = parts[nodeModulesIndex + 1].replace(/[^a-zA-Z0-9_-]/g, '-'); // Sanitize name
+        }
         const result = await esbuild({
           ...config.esbuildOptions,
           splitting: false,
@@ -19,9 +29,12 @@ export const cssPlugin = {
           loader: { '.svg': 'dataurl', '.gif': 'dataurl', '.png': 'dataurl', '.webp': 'dataurl' },
           define: undefined,
         });
+        const processedCss = result.outputFiles?.[0].text ?? "";
         const contents = `
       const styles = new CSSStyleSheet();
-      styles.replaceSync(\`${result.outputFiles?.[0].text ?? ""}\`);
+      styles.replaceSync(\`
+      ${layerName ? `@layer ${layerName} {\n${processedCss}\n}` : processedCss}
+      \`);
       export default styles;`;
         return { contents };
       } catch (ex) {
